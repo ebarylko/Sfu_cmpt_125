dc2e0ffcd83e8921df131827df91e828ffdc3235
Eitan <eitan@barylko.com>
2022-10-25 21:52:12 -0700

    Added cut_space function

--- Midterm1_practice/sorting_algorithms.c
+++ Midterm1_practice/sorting_algorithms.c
@@ -185,6 +185,7 @@ student* create_student(int id, char* name) {
     new_student->ID = id;
     new_student->name = name;
     new_student->grades_len = 0;
+    new_student->grades = NULL;
     return new_student;
 }
 
@@ -200,6 +201,254 @@ int find_grade(student* s, char* courseID) {
             student_grade = s->grades[i].grade;
     }
     return student_grade;
+}
+
+// new memory = initial memory + memoery for one class
+// sizeof(student) + grades_len * sizeof(course_grade)
+int add_grade(student* student, course_grade new_grade) {
+    int grade = find_grade(student, new_grade.courseID);
+    if (grade != -1) {
+        return 0;
+    }
+    int classes = student->grades_len;
+    student->grades = (course_grade*)realloc(student->grades,  sizeof(course_grade) * (classes + 1));
+    if (student->grades) {
+        student->grades_len++;
+        student->grades[classes] = new_grade;
+        return 1;
+    }
+    return 0;
+}
+
+
+/**
+ *Write a function that gets a pointer to a student and a course grade, and adds
+the grade to the student's array of grades.
+If student->grades already contains a course with this courseID, the function does nothing.
+If student->grades==NULL, you need to create an array of length 1 and add the new grade into
+the array.
+If student->grades!=NULL, you need to increase the size of the array by one, and add
+new_grade into the new entry in the array of grades.
+If a grade is added the function returns 1. Otherwise the function returns 0; this can be
+because the course is already there or malloc fails. @brief 
+**/
+
+course_grade new_class(char* class_name, int grade) {
+    course_grade class;
+    class.courseID = class_name;
+    class.grade = grade;
+    return class;
+}
+
+
+void add_course_test() {
+    student* paul = create_student(14, "Paul");
+    course_grade class = new_class("CRIM140", 97);
+    int add_req = add_grade(paul, class);
+    if (add_req) {
+        printf("success with adding a class\n");
+        printf("new class: %s\n", paul->grades[0].courseID);
+    } else  {
+        printf("faliure adding a class\n");
+        return;
+    }
+    class = new_class("MACM1010", 78);
+    add_req = add_grade(paul, class);
+    if (add_req) {
+        printf("success with adding a class\n");
+        printf("new class: %s\n", paul->grades[1].courseID);
+    } else  {
+        printf("faliure adding a class");
+        return;
+}
+    add_req = add_grade(paul, class);
+    if (add_req) {
+        printf("success with adding a class\n");
+        printf("new class: %s\n", paul->grades[1].courseID);
+    } else  {
+        printf("faliure adding a class\n");
+        return;
+}
+}
+
+
+// bool is_sorted(int* A, int n){ 
+//     for (int i = 0; i < n - 1; i++) {
+//         if (A[i] > A[i + 1])
+//             return 0;
+//     }
+//     return 1;
+// }
+
+
+int compiles() {
+char str[10] ={'a','b','c',0,'1','2'};
+char* ptr = str;
+printf("%s\n", ptr+1);
+return 0;
+}
+
+
+
+
+
+/**
+ * Write a function that gets two strings and computes the length of their longest
+common prefix. For example,
+- longest_common_prefix(“abcd”, “ab12”) is 2 - the prefix is “ab”
+- longest_common_prefix(“abcd”, “cd”) is 0 - the prefix is empty
+- longest_common_prefix(“abcd”, “abcdefg”) is 4 - the prefix is “abcd”@brief 
+ * 
+ * @return int 
+ */
+// go until one of the characters do not match, or i exceeds length of one of the 
+//string
+// outside: n elements checked for strlen, o(1) for assignment
+// loop: O(1) * prefix times, worst case prefix is all of string = n
+// total, O(N) in total
+int longest_common_prefix(const char* str1, const char* str2) {
+    int pos = 0;
+    int prefix = 0;
+    while (str1[pos] != 0 && str2[pos != 0]) {
+        prefix++;
+        pos++;
+    }
+    return prefix;
+}
+
+
+/**
+//  Write a function that gets a parameter n. It reads n ints from the user (using
+scanf), and prints the numbers in the reverse order.
+ * 
+ */
+void read_and_print_reverse(int n) {
+    int* reverse_nums = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        printf("Enter a number:");
+        scanf("%d", &reverse_nums[n - 1 - i]);
+    }
+    for(int i = n - 1; i >= 0; i--) {
+        printf("%d-", reverse_nums[i]);
+    }
+    free(reverse_nums);
+}
+
+/**
+ * Write a function that gets an array of ints of length n, and returns an array of
+length n, such that the output[i] is equal to the maximal element in the input subarray [0,..., i].
+For example,
+- input [1, 4, 3, 8, 2, 9]
+- output [1, 4, 4, 8, 8, 9].@brief 
+ * 
+ * @param ar 
+ * @param n 
+ * @return int* 
+ */
+
+int* max_prefixes(int* ar, int n) {
+    if (n == 0)
+        return NULL;
+    int* new_arr = (int*)malloc(n * sizeof(int));
+    if (!new_arr) 
+        return NULL;
+    int max = ar[0];
+    new_arr[0] = max;
+    for (int pos = 1; pos < n; pos++) {
+        if (ar[pos] > max) {
+            max = ar[pos];
+        }
+        new_arr[pos] = max;
+    }
+    return new_arr;
+}
+
+void max_pref_test() {
+    int tst_arr[] = {1, 2, 3, 5, 4, 3, 0};
+    int tst_arr2[] = {};
+    int tst_arr1[] = {1};
+    int* max_arr = max_prefixes(tst_arr, 7);
+    int* max_arr2 = max_prefixes(tst_arr1, 1);
+    int* max_arr3 = max_prefixes(tst_arr2, 0);
+
+    for (int i = 0; i < 6; i++) {
+        printf("%d, ", max_arr[i]);
+    }
+    printf("\n");
+    print_array(max_arr2, 1);
+    free(max_arr);
+    free(max_arr2);
+    free(max_arr2);
+}
+
+void reverse_arr_test() {
+    read_and_print_reverse(0);
+}
+
+void prefix_test() {
+    int test_1 = longest_common_prefix("hello", "help");
+    int test_2 = longest_common_prefix("hello", "");
+    int test_3 = longest_common_prefix("hello", "hello");
+    int test_4 = longest_common_prefix("hello", "wernekie");
+    printf("\n%d, %d, %d, %d\n", test_1, test_2, test_3, test_4);
+}
+
+// func that checks for amount of white space, them shifts characters appropiate amount
+// of space. if char not null char, repeats on rest of string
+// takes string, where to start in string, and how many characters to shift 
+// accounting for previous white spaces
+void cut_space(char* str, int pos, int spaces) {
+    while (str[pos] == 32) {
+        spaces++;
+        pos++;
+    }
+    while (str[pos] != 0 || str[pos] != 32) {
+        str[pos - spaces] = str[pos];
+        pos++;
+    }
+    if (str[pos] == 0) {
+        str[pos - spaces] = str[pos];
+        return;
+    } else {
+        cut_space(str, pos, spaces);
+    }
+}
+
+
+
+// write a function that removes spaces from a string
+// check if there is a white space or null char. if null char, stop.
+// "a b x", "abc", 
+// else, implement until the end of string
+void rem_space(char* str) {
+    int pos = 0;
+    int len = strlen(str);
+    // moves to end of string or first white space
+    while  (str[pos] != 32 && str[pos]) 
+        pos++;
+        //if no white space
+    if (pos == len){
+        return;
+    } else {
+        cut_space(str, pos, 0);
+    }
+}
+
+void mod_string_test() {
+    char test[] = "hello";
+    char test1[] = "  hello";
+    char test2[] = "";
+    char test3[] = "a b x";
+    // rem_space(test1);
+    rem_space(test2);
+    // rem_space(test3);
+    // printf("%s\n%s\n%s\n%s\n", test, test1, test2, test3);
+    // printf("%c---", test[5]);
+    printf("%s---", test2);
+
+    // printf("%d", len);
+    // printf("%c\n%c\n%c\n%c\n", test[0], test1[0], test2[0], test3[0]);
+
 }
 
 
@@ -231,7 +480,12 @@ int main() {
     james->grades_len = 2;
     james->grades = grades; 
     int grade = find_grade(james, "ARCH130");
-    printf("Grade is %d\n", grade);
-    // printf("ID: %d, name: %s, grades_len: %d", james->ID, james->name, james->grades_len);
+    // printf("Grade is %d\n", grade);
+
+    add_course_test();
+//    prefix_test();
+//    reverse_arr_test();
+//    max_pref_test();
+    mod_string_test();
     return 0;
 }
