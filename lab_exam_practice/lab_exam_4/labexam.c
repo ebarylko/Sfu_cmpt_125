1a004c929e7a3a8e68830c12c1dd732cce26fb5e
Eitan <eitan@barylko.com>
2022-11-07 13:18:16 -0800

    Added fourth practice lab exam

---
+++ lab_exam_practice/lab_exam_4/labexam.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "labexam.h"
+
+int count_hyphens_substrings(const char* str) {
+  // implement me
+  return -1;
+}
+
+int countif(LL_t* list, bool(*pred)(int)) {
+  // implement me
+  return -1;
+}
+
+stack_t* stack_copy(stack_t* orig) {
+  // implement me
+  return NULL;
+}
+
