dbea9911ecd5749930b03f6d1836e79074b26dcc
Eitan <eitan@barylko.com>
2022-11-06 17:39:10 -0800

    Implemented first function

--- lab_exam_practice/lab_exam_5/labexam.c
+++ lab_exam_practice/lab_exam_5/labexam.c
@@ -5,10 +5,38 @@
 
 #include "labexam.h"
 
+int lower_vowel(char c) {
+  if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+    return 1;
+  }
+  return 0;
+}
 
+// change lowercase vowels, return number of changes made
+int modify(char* str) {
+  int changes = 0;
+  int pos = 0;
+  while (str[pos]) {
+    if (lower_vowel(str[pos])) {
+      str[pos] = '*';
+      changes++;
+    }
+    pos++;
+  }
+  return changes;
+}
+
+// modifies all lowercase vowels into asterisks
+// returns number of changes made
+// go over each string and check if char is lowercase vowel,
+// if so modify it, and increment counter
+//return counter at end
 int vowels2asterisks(char* ar[], int n) {
-  // implement me
-  return -1;
+  int changes = 0;
+  for (int pos = 0; pos < n; pos++) {
+    changes += modify(ar[pos]);
+  }
+  return changes;
 }
 
 
