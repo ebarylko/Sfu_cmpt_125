a920836c29b2ea71ad5222ef00520d9e12415eca
Eitan <eitan@barylko.com>
2022-11-06 17:03:44 -0800

    Added fourth practice lab examl

---
+++ lab_exam_practice/lab_exam_5/labexam.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "labexam.h"
+
+
+int vowels2asterisks(char* ar[], int n) {
+  // implement me
+  return -1;
+}
+
+
+int longest_equal_seq(const int* ar, int n) {
+  // implement me
+  return -1;
+}
+
+
+void queues_swap(queue_t* q1, queue_t* q2) {
+  // implement me
+  return;
+}  
