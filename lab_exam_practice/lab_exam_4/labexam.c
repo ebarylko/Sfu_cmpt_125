b84360360904d294df734e06a19bb35bdee85342
Eitan <eitan@barylko.com>
2022-11-07 20:51:10 -0800

    Implemented function two and three

--- lab_exam_practice/lab_exam_4/labexam.c
+++ lab_exam_practice/lab_exam_4/labexam.c
@@ -5,18 +5,76 @@
 
 #include "labexam.h"
 
+// return pos of where hyphen is no longer found or end of string
+//"--aabc-----hi***"
+int find_hyphens(const char* str, int pos) {
+  // lookiing for apperance of hyphen
+  while (str[pos] && str[pos] != '-') {
+    pos++;
+  }
+
+  int matches = 0;
+  if (str[pos]) {
+    //count hyphens
+    while (str[pos] == '-') {
+      pos++;
+      matches++;
+    }
+  }
+  return matches;
+}
+
+/**
+ *Write a function that gets a string and returns the 
+ number of substrings consisting of hyphens only 
+ */
 int count_hyphens_substrings(const char* str) {
-  // implement me
-  return -1;
+  // int length = strlen(str);
+  int matches = 0;
+  int hyphens = 0;
+  int pos = 0;
+  printf("hyphens:%d\n", find_hyphens("a!ba-ei-XXoi---", 9));
+  while ((matches = find_hyphens(str, pos)) > 0) {
+    hyphens++;
+    pos += matches;
+  }
+  return hyphens;
 }
 
 int countif(LL_t* list, bool(*pred)(int)) {
-  // implement me
-  return -1;
+  int matches = 0;
+  node_t* curr = list->head;
+  while (curr) {
+    if (pred(curr->data)) 
+      matches++;
+    curr = curr->next;
+  }
+  return matches;
 }
+/**
+ *gets a stack orig, and creates a new stack with the same content
+// when the function returns, orig needs to be in its initial state @brief 
+ */
+// make a second stack, copy all values from the first stack
+// onto here. will be in reverse order.a
+// create third stack to reorder elements by inserting them.
+// while second stack ! empty, continue to add vals
+// free second stack, return third stack.
 
-stack_t* stack_copy(stack_t* orig) {
-  // implement me
-  return NULL;
+stack_tt* stack_copy(stack_tt* orig) {
+  stack_tt* unordered_s = stack_create();
+  stack_tt* ordered_s = stack_create();
+  node_t* curr = orig->list->head;
+  while (curr) {
+    stack_push(unordered_s, curr->data);
+    curr = curr->next;
+  }
+
+  while (!(stack_is_empty(unordered_s))) {
+    stack_push(ordered_s, stack_pop(unordered_s));
+  }
+
+  free(unordered_s);
+  return ordered_s;
 }
 
