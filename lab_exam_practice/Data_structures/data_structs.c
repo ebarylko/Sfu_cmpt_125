92b0a240da1f170dbc9f5b2b2f07ff354a6608b0
Eitan <eitan@barylko.com>
2022-11-09 14:29:28 -0800

    Implemented stack and queue

--- lab_exam_practice/Data_structures/data_structs.c
+++ lab_exam_practice/Data_structures/data_structs.c
@@ -45,7 +45,7 @@ return list->head;
 
 
 bool has_next(linked_list_iterator iter) {
-return iter && iter->next;
+return iter;
 }
 
 int get_elem(linked_list_iterator iter) {
@@ -73,7 +73,13 @@ node* create_node(int val) {
 
 // get size of list
 int get_size(linked_list* list) {
-    return list->elems;
+    int size = 0;
+    linked_list_iterator iter = create_iterator(list);
+    while (has_next(iter)) {
+        size++;
+        iter = next(iter);
+    }
+    return size;
 }
 
 // adds to head
@@ -87,10 +93,6 @@ void add_to_head(linked_list* list, int val) {
     (list->elems)++;
 }
 
-// if empty, add and make head and tail point to same place
-// make tail point to new pointer, and make new pointer be tail
-// increment size of list
-// if empty 
 //adds to tail
 void add_to_tail(linked_list* list, int val) {
     node* node = create_node(val);
@@ -104,16 +106,10 @@ void add_to_tail(linked_list* list, int val) {
     (list->elems)++;
 }
 
-
-// if empty, do nothing
-// else, assign another variable of the head, and make the element in front of
-// the head be the new head. free the old head, and decrement the size of the list
-// if only one element, make the tail null after
 // gives all elements after head, returns taken element
 int rest(linked_list* list) {
     if (!list->elems)
         return -1;
-
 
     node* old_head = list->head;
     int val = old_head->data;
@@ -128,12 +124,11 @@ int rest(linked_list* list) {
 }
 
 //remove element from tai
-// if only one element, made head and tail point to the same place
-// if not, traverse until tail, free tail, set new tail, decrement size
+int remove_tail(linked_list* list){ 
+    if (!list->elems)
+        return -1;
 
-void remove_tail(linked_list* list){ 
-    if (!(list->elems))
-        return;
+    int val = list->tail->data;
 
     if (list->elems == 1) {
         free(list->head);
@@ -148,8 +143,70 @@ void remove_tail(linked_list* list){
     curr->next = NULL;
     list->tail = curr;
     }
-    (list->elems)--;
+
+    list->elems--;
+    return val;
 }
+
+void free_list(linked_list* list) {
+    linked_list_iterator iter = create_iterator(list);
+    while(has_next(iter)) {
+        next(iter);
+        rest(list);
+    }
+    free(list);
+}
+
+
+
+// queue
+
+typedef linked_list queue;
+
+queue* create_queue() {
+    queue* new_queue = create_list();
+    return new_queue;
+}
+
+void free_queue(queue* queue) {
+    free_list(queue);
+}
+
+int queue_size(queue* queue) {
+    return get_size(queue);
+}
+
+void enqueue(queue* queue, int val) {
+    add_to_tail(queue, val);
+}
+
+int dequeue(queue* queue) {
+    return rest(queue);
+}
+
+typedef linked_list stack;
+
+queue* create_stack() {
+    queue* new_stack = create_list();
+    return new_stack;
+}
+
+void free_stack(stack* stack) {
+    free_list(stack);
+}
+
+int stack_size(stack* stack) {
+    return get_size(stack);
+}
+
+void push(stack* stack, int val) {
+    add_to_head(stack, val);
+}
+
+int pop(stack* stack) {
+    return rest(stack);
+}
+
 
 //practice problems
 /**
@@ -198,6 +255,59 @@ void list_exists_test() {
         printf("The list is not valid");
 }
 
+void iter_test() {
+    linked_list* list1 = create_list();
+    linked_list* list2 = create_list();
+    linked_list* list3 = create_list();
+
+    add_to_head(list2, 5);
+    add_to_head(list2, 6);
+    add_to_head(list2, 3);
+    add_to_tail(list3, 5);
+    add_to_tail(list3, 6);
+    add_to_tail(list3, 3);
+    add_to_tail(list3, 3);
+
+
+    if (get_size(list1) == 0 && get_size(list2) == 3 && 
+    get_size(list3) == 4) {
+        printf("\n Iteration through linked list worked");
+    } else 
+        printf("Iteration through linked list failed\n");
+}
+
+//check to see if functions of a queue works
+void queue_test() {
+    queue* queue1 = create_queue();
+    queue* queue2 = create_queue();
+
+    enqueue(queue1, 2);
+    enqueue(queue1, 3);
+    enqueue(queue1, 4);
+
+    printf("Precheck\n");
+    //enqueue check
+    if (queue_size(queue1) == 3 && get_size(queue2) == 0) {
+        printf("\n enqueue check worked");
+    } 
+
+    printf("\nPostcheck\n");
+
+    
+    dequeue(queue1);
+    dequeue(queue1);
+    //dequeue check
+    int val;
+    if (get_size(queue1) == 1 && (val = dequeue(queue2)) == -1) {
+        printf("\n dequeue check worked");
+    }  else
+        printf("%d", val);
+
+    free_queue(queue1);
+    free_queue(queue2);
+}
+
+
 void add_node_test() {
     linked_list* list = create_list();
     linked_list* list2 = create_list();
@@ -229,22 +339,25 @@ void remove_node_test() {
     add_to_head(list, 3);
 
     add_to_tail(list2, 5);
-    // add_to_tail(list2, 6);
-    // add_to_tail(list2, 3);
+    add_to_tail(list2, 6);
+    add_to_tail(list2, 3);
 
     rest(list);
     remove_tail(list2);
+
     if (list->elems == 2 && list->head->data == 6 && list->tail->data == 5) {
         printf("\nHead_removal worked");
-    } else 
+    } else  {
         printf("\nHead_removal failed");
         printf("\n %d, head: %d, tail: %d", list->elems, list->head->data, list->tail->data);
+    }
 
-    if (list2->elems == 0 && list2->head == list2->tail) {
-        printf("\nHead_removal worked for list 2");
-    } else 
-        printf("\nHead_removal failed for list 2");
+    if (list2->elems == 2 && list2->head->data == 5 && list2->tail->data == 6) {
+        printf("\ntail_removal worked for list 2");
+    } else  {
+        printf("\ntail_removal failed for list 2");
         printf("\n %d, head: %d, tail: %d", list2->elems, list2->head, list2->tail);
+    }
 }
 
 void mid_list_test() {
@@ -264,9 +377,11 @@ void mid_list_test() {
 
 
 int main() {
-    list_exists_test();
-    add_node_test();
-    remove_node_test();
+    // list_exists_test();
+    // add_node_test();
+    // remove_node_test();
     // mid_list_test();
+    // iter_test();
+    queue_test();
     return 0;
 }
