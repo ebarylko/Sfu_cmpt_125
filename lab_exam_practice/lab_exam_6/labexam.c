e9477f8ed8eb4d6084cfbff20842610cf44df59d
Eitan <eitan@barylko.com>
2022-11-09 19:22:45 -0800

    Added sixth lab exam

---
+++ lab_exam_practice/lab_exam_6/labexam.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "labexam.h"
+
+
+void str_right_rotate(char* str) {
+  // implement me
+}
+
+
+int* map_to_new_array(const int* ar, int n, int(*foo)(int)) {
+  // implement me
+  return NULL;
+}
+
+
+void stacks_swap(stack_t* s1, stack_t* s2) {
+  // implement me
+  return;
+}
+
