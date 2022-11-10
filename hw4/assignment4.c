91b0ab8c9dfea9b20aa874ce61306e4d5e744ad3
Eitan <eitan@barylko.com>
2022-11-10 07:31:06 -0800

    Implemented equal queues

--- hw4/assignment4.c
+++ hw4/assignment4.c
@@ -1,5 +1,13 @@
 #include "assignment4.h"
 
+int transfer_queue(queue_str_t* q1, queue_str_t* q2) {
+  int size = 0;
+  while (!(queue_is_empty(q1))) {
+    enqueue(q2, dequeue(q1));
+    size++;
+  }
+  return size;
+}
 
 /* Question 1 */
 /**
@@ -14,15 +22,10 @@
 // return val
 int queue_size(queue_str_t* q) {
   queue_str_t* copy = queue_create();
-  int size = 0;
-  // move elements over to get the size
-  while (!(queue_is_empty(q))) {
-    enqueue(copy, dequeue(q));
-    size++;
-  }
-  //return elements back to original queeue
-  while (!(queue_is_empty(copy))) 
-    enqueue(q, dequeue(copy));
+  //get size of queue
+  int size = transfer_queue(q, copy);
+  //return queue back
+  transfer_queue(copy, q);
 
   queue_free(copy);
   return size;
@@ -50,6 +53,36 @@ int queue_size(queue_str_t* q) {
 bool queue_equal(queue_str_t* q1, queue_str_t* q2) {
   if (queue_size(q1) != queue_size(q2))
     return false;
+
+queue_str_t* q1_copy = queue_create();
+queue_str_t* q2_copy = queue_create();
+int match = 1;
+char* str1;
+char* str2;
+
+// checking the strings from both queues
+while ( !(queue_is_empty(q1)) && !(queue_is_empty(q2)) && match) {
+// storing strings
+str1 = dequeue(q1);
+str2 = dequeue(q2);
+
+enqueue(q1_copy, str1);
+enqueue(q2_copy, str2);
+
+// if strings don't match
+if (strcmp(str1, str2)) 
+  match = 0;
+}
+// if not equal before end, transfer remaining elements
+transfer_queue(q1, q1_copy);
+transfer_queue(q2, q2_copy);
+// return elements in coorrect order back to queues
+transfer_queue(q1_copy, q1);
+transfer_queue(q2_copy, q2);
+
+queue_free(q1_copy);
+queue_free(q2_copy);
+return match;
 
 }
 
